868ceb141c1de7e18a4376469c33230133d1b6f0
takkyuuplayer <takkyuuplayer@gmail.com>
2022-03-21 22:40:45 +0900

    Move state to s3

--- main.ts
+++ main.ts
@@ -12,7 +12,6 @@ class StateStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
     const aws = new AwsProvider(this, "aws", {
       region: "ap-northeast-1",
     });
@@ -21,97 +20,92 @@ class StateStack extends TerraformStack {
       key: "tf-infra/cdktf.tfstate",
       region: aws.region,
     });
+
+    Cloudflare(this);
   }
 }
 
-class CloudflareStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+function Cloudflare(scope: Construct) {
+  new CloudflareProvider(scope, "cloudflare");
 
-    new CloudflareProvider(this, "cloudflare");
-    new AwsProvider(this, "aws", {
-      region: "ap-northeast-1",
-    });
-    const zone = new DataCloudflareZone(this, "takkyuuplayer.com", {
-      name: "takkyuuplayer.com",
-    });
+  const zone = new DataCloudflareZone(scope, "takkyuuplayer.com", {
+    name: "takkyuuplayer.com",
+  });
 
-    [
-      {
-        priority: 1,
-        value: "aspmx.l.google.com",
-      },
-      {
-        priority: 5,
-        value: "alt1.aspmx.l.google.com",
-      },
-      {
-        priority: 5,
-        value: "alt2.aspmx.l.google.com",
-      },
-      {
-        priority: 10,
-        value: "alt3.aspmx.l.google.com",
-      },
-      {
-        priority: 10,
-        value: "alt4.aspmx.l.google.com",
-      },
-    ].forEach((record) => {
-      new Record(this, record.value, {
-        name: "@",
-        zoneId: zone.zoneId,
-        type: "MX",
-        priority: record.priority,
-        value: record.value,
-      });
-    });
-
-    new Record(this, "spf", {
+  [
+    {
+      priority: 1,
+      value: "aspmx.l.google.com",
+    },
+    {
+      priority: 5,
+      value: "alt1.aspmx.l.google.com",
+    },
+    {
+      priority: 5,
+      value: "alt2.aspmx.l.google.com",
+    },
+    {
+      priority: 10,
+      value: "alt3.aspmx.l.google.com",
+    },
+    {
+      priority: 10,
+      value: "alt4.aspmx.l.google.com",
+    },
+  ].forEach((record) => {
+    new Record(scope, record.value, {
       name: "@",
       zoneId: zone.zoneId,
-      value:
-        "v=spf1 include:_spf.google.com include:_amazonses.takkyuuplayer.com ~all",
-      type: "TXT",
+      type: "MX",
+      priority: record.priority,
+      value: record.value,
     });
-    new Record(this, "dmarc", {
-      name: "_dmarc",
-      zoneId: zone.zoneId,
-      value: "v=DMARC1; p=none; rua=mailto:takkyuuplayer@gmail.com",
-      type: "TXT",
-    });
+  });
 
-    [
+  new Record(scope, "spf", {
+    name: "@",
+    zoneId: zone.zoneId,
+    value:
+      "v=spf1 include:_spf.google.com include:_amazonses.takkyuuplayer.com ~all",
+    type: "TXT",
+  });
+  new Record(scope, "dmarc", {
+    name: "_dmarc",
+    zoneId: zone.zoneId,
+    value: "v=DMARC1; p=none; rua=mailto:takkyuuplayer@gmail.com",
+    type: "TXT",
+  });
+
+  [
+    {
+      fqdn: "takkyuuplayer.com",
+      cloudfrontId: "E1G6FSVIWAIZ4",
+      name: "@",
+    },
+    {
+      fqdn: "www.takkyuuplayer.com",
+      cloudfrontId: "E3HUV11AFSHUKJ",
+      name: "www",
+    },
+  ].forEach((record) => {
+    const cf = new DataAwsCloudfrontDistribution(
+      scope,
+      `Cloudfront/${record.fqdn}`,
       {
-        fqdn: "takkyuuplayer.com",
-        cloudfrontId: "E1G6FSVIWAIZ4",
-        name: "@",
-      },
-      {
-        fqdn: "www.takkyuuplayer.com",
-        cloudfrontId: "E3HUV11AFSHUKJ",
-        name: "www",
-      },
-    ].forEach((record) => {
-      const cf = new DataAwsCloudfrontDistribution(
-        this,
-        `Cloudfront/${record.fqdn}`,
-        {
-          id: record.cloudfrontId,
-        }
-      );
-      new Record(this, `CNAME/${record.fqdn}`, {
-        name: record.name,
-        zoneId: zone.zoneId,
-        value: cf.domainName,
-        type: "CNAME",
-        proxied: true,
-      });
+        id: record.cloudfrontId,
+      }
+    );
+    new Record(scope, `CNAME/${record.fqdn}`, {
+      name: record.name,
+      zoneId: zone.zoneId,
+      value: cf.domainName,
+      type: "CNAME",
+      proxied: true,
     });
-  }
+  });
 }
 
 const app = new App();
 new StateStack(app, "tf-infra-state");
-new CloudflareStack(app, "tf-infra-cloudflare");
 app.synth();
