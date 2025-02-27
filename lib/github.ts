907b462fd1f4f335f2d34830f400511bd3ae3338
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2025-02-28 01:23:20 +0900

    Add AppID, AppSecrets for CI (#27)

---
+++ lib/github.ts
@@ -0,0 +1,42 @@
+import { ActionsSecret } from "@cdktf/provider-github/lib/actions-secret";
+import { GithubProvider } from "@cdktf/provider-github/lib/provider";
+import { TerraformVariable } from "cdktf";
+import { Construct } from "constructs";
+
+export function GitHub(scope: Construct) {
+  // https://github.com/peter-evans/create-pull-request/blob/main/docs/concepts-guidelines.md#authenticating-with-github-app-generated-tokens
+  const githubCiAppId = new TerraformVariable(scope, "github_ci_app_id", {
+    type: "string",
+  });
+  const githubCiAppPrivateKey = new TerraformVariable(
+    scope,
+    "github_ci_app_private_key",
+    {
+      type: "string",
+    }
+  );
+
+  new GithubProvider(scope, "github", {
+    token: process.env.GITHUB_TOKEN,
+    owner: "takkyuuplayer",
+  });
+
+  [
+    "go-exercise",
+    "homepage4.0",
+    "rb-exercise",
+    "rs-exercise",
+    "ts-exercise",
+  ].forEach((repo) => {
+    new ActionsSecret(scope, `${repo}/app_id`, {
+      repository: repo,
+      secretName: "CI_APP_ID",
+      plaintextValue: githubCiAppId.value,
+    });
+    new ActionsSecret(scope, `${repo}/app_private_key`, {
+      repository: repo,
+      secretName: "CI_APP_PRIVATE_KEY",
+      plaintextValue: githubCiAppPrivateKey.value,
+    });
+  });
+}
